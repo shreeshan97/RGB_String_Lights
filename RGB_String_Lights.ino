8fabdea9c149539049e1dc6a09cc9e93b996fd67
Shreesha N <shreesha.nakshathri@gmail.com>
2025-04-20 23:52:49 +0530

    Testing ESP32C3 with 2s blinkly and Serial messages

--- RGB_String_Lights.ino
+++ RGB_String_Lights.ino
@@ -7,11 +7,17 @@ bool state = false;
 void setup() 
 {
   pinMode(BOARD_LED, OUTPUT);
+  Serial.begin(115200);
 }
 
 void loop() 
 {
   state = !state;
   digitalWrite(BOARD_LED, state);
+  Serial.print("LED state : ");
+  if(state)
+    Serial.println("OFF");
+  else
+    Serial.println("ON");
   delay(2000U);
 }
