2a871f9824ea4e2940e03f3b0ce4272f214cbdae
Shreesha N <shreesha.nakshathri@gmail.com>
2025-04-21 22:50:14 +0530

    Testing Addressable LED (WS2812) Blinky with 0.5s delay

--- RGB_String_Lights.ino
+++ RGB_String_Lights.ino
@@ -1,23 +1,29 @@
+#include <FastLED.h>
 
 #define BOARD_LED 8
+#define DATA_PIN 4
+#define NUM_LEDS 1
 
-bool state = false;
-
+CRGB leds[NUM_LEDS];
 
 void setup() 
 {
   pinMode(BOARD_LED, OUTPUT);
   Serial.begin(115200);
+  FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS); 
+  FastLED.setBrightness(200);
 }
 
 void loop() 
 {
-  state = !state;
-  digitalWrite(BOARD_LED, state);
-  Serial.print("LED state : ");
-  if(state)
-    Serial.println("OFF");
-  else
-    Serial.println("ON");
-  delay(2000U);
+  // Turn the LED on, then pause
+  digitalWrite(BOARD_LED, LOW);
+  leds[0] = CRGB::Purple;
+  FastLED.show();
+  delay(500);
+  // Now turn the LED off, then pause
+  digitalWrite(BOARD_LED, HIGH);
+  leds[0] = CRGB::Black;
+  FastLED.show();
+  delay(500);
 }
